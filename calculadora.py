1520da05db1dc514578085219e10b24dcdd46375
Xavi <xavi.quesada@iticbcn.cat>
2023-10-10 18:56:12 +0200

    doctest afegits

--- calculadora.py
+++ calculadora.py
@@ -6,18 +6,21 @@ Calculadora basica
 
 __author__ = "Xavi"
 
-class Calculadora():
-    def __init__(self):
-        super.__init__()
     
-    def suma(a, b):
-        return a+b
-    
-    def resta(a, b):
-        return a-b
-    
-    def multiplica(a, b):
-        return a*b
-    
-    def divisio(a, b):
-        return a/b
+def suma(a, b):
+    """
+    >>> suma(3, 5)
+    8
+    >>> suma(0, 0)
+    0
+    """
+    return a+b
+
+def resta(a, b):
+    return a-b
+
+def multiplica(a, b):
+    return a*b
+
+def divisio(a, b):
+    return a/b
