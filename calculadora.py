87a5b8242e653ae2a09bcce3c687940f34ac7185
xavi <f.javier.quesada@iesjoandaustria.org>
2023-10-10 11:02:24 +0200

    Commit inicial

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Calculadora basica
+"""
+
+__author__ = "Xavi"
+
+class Calculadora():
+    def __init__(self):
+        super.__init__()
+    
+    def suma(a, b):
+        return a+b
+    
+    def multiplica(a, b):
+        return a*b
